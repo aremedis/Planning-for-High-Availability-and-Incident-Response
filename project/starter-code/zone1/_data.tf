c3a01859f531b37bb4021a2a76d3e110a2f763b1
Aremedis <aremedis@gmail.com>
2023-05-17 08:54:26 -0500

    fix Database creation errors in terraform

--- project/starter-code/zone1/_data.tf
+++ project/starter-code/zone1/_data.tf
@@ -2,11 +2,11 @@ data "aws_caller_identity" "current" {}
 
  data "aws_ami" "amazon_linux_2" {
    most_recent = true
-   owners      = ["873368230709"]
+   owners      = ["amazon"]
 
    filter {
      name   = "owner-alias"
-     values = ["873368230709"]
+     values = ["amazon"]
    }
 
 
